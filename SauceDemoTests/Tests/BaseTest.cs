e681aed66eae0fecc7f3cf6f4765e7f0e76e6e58
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-25 07:57:33 +0800

    Add BaseTest class for all tests.

---
+++ SauceDemoTests/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SauceDemoTests.Utils;
+
+namespace SauceDemoTests.Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Driver setup
+            _driver = WebDriverFactory.CreateDriver("chrome");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
