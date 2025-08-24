a7f882871c6b0ee81f742413f3990e64ab123ca2
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-25 07:50:28 +0800

    Add WebDriverFactory for chrome and edge drivers.

---
+++ SauceDemoTests/Utils/WebDriverFactory.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace SauceDemoTests.Utils
+{
+    public static class WebDriverFactory
+    {
+        public static IWebDriver CreateDriver(string driver)
+        {
+            switch (driver.ToLower())
+            {
+                case "chrome":
+                    var chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("start-maximized");
+                    return new ChromeDriver(chromeOptions);
+
+                case "edge":
+                    var edgeOptions = new EdgeOptions();
+                    edgeOptions.AddArgument("start-maximized");
+                    edgeOptions.AddArgument("--headless");
+                    return new EdgeDriver(edgeOptions);
+
+                default:
+                    throw new ArgumentException("Invalid browser name. Please enter 'chrome' or 'edge'.");
+            }
+        }
+    }
+}
