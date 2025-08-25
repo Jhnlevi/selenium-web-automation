2b1e00e1ad0db95fe852497775766ee78603b6cf
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-25 08:05:02 +0800

    Add WebDriverExtension method for waiting elements to be visible.

---
+++ SauceDemoTests/Utils/WebDriverExtensions.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SauceDemoTests.Utils
+{
+    public static class WebDriverExtensions
+    {
+        // Waits for element to be visible
+        public static IWebElement WaitForElementVisible(this IWebDriver driver, By locator, int timeoutInSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+
+            return wait.Until(driv =>
+            {
+                try
+                {
+                    var element = driv.FindElement(locator);
+                    return (element.Displayed && element.Enabled) ? element : null;
+                }
+                catch (NoSuchElementException)
+                {
+                    return null;
+                }
+            });
+        }
+    }
+}
