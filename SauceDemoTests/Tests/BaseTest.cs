1e68fde362094d2a34557926a5ab6f32b56215d1
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-26 05:22:21 +0800

    Modify base test file for reports.

--- SauceDemoTests/Tests/BaseTest.cs
+++ SauceDemoTests/Tests/BaseTest.cs
@@ -1,17 +1,31 @@
-﻿using OpenQA.Selenium;
+﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
 using SauceDemoTests.Utils;
 
 namespace SauceDemoTests.Tests
 {
     public class BaseTest
     {
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
         protected IWebDriver _driver;
+
+        // Report
+        [OneTimeSetUp]
+        public void SetupReport()
+        {
+            _extent = ReportManager.GetInstance();
+        }
 
         [SetUp]
         public virtual void SetUp()
         {
             // Driver setup
             _driver = WebDriverFactory.CreateDriver("chrome");
+
+            // Report test setup
+            string reportTestName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(reportTestName);
         }
 
         [TearDown]
@@ -19,6 +33,13 @@ namespace SauceDemoTests.Tests
         {
             _driver.Quit();
             _driver.Dispose();
+        }
+
+        // Report
+        [OneTimeTearDown]
+        public void CloseReport()
+        {
+            _extent.Flush();
         }
     }
 }
