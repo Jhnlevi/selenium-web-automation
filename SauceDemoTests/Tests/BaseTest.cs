932268d7518c6101aae59ae15688da8b159a50c8
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-27 16:07:56 +0800

    Refactor BaseTest file.

--- SauceDemoTests/Tests/BaseTest.cs
+++ SauceDemoTests/Tests/BaseTest.cs
@@ -6,50 +6,58 @@ namespace SauceDemoTests.Tests
 {
     public class BaseTest
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Structure",
+            "NUnit1032:An IDisposable field/property should be Disposed in a TearDown method",
+            Justification = "Dispose and Quit are handled by CloseDriver method.")]
+        protected IWebDriver _driver;
         protected ExtentReports _extent;
         protected ExtentTest _test;
-        protected IWebDriver _driver;
-        private string reportName = null!;
+        private TestContext _context = TestContext.CurrentContext;
 
         [OneTimeSetUp]
         public void SetupReport()
         {
-            // Report set up
-            reportName = TestContext.CurrentContext.Test.Name;
+            // Report set up.
+            var reportName = _context.Test.Name;
             _extent = ReportManager.CreateReport(reportName);
         }
 
         [SetUp]
         public virtual void SetUp()
         {
-            // Driver set up
-            _driver = WebDriverFactory.CreateDriver("chrome");
+            // Driver set up.
+            _driver = WebDriverFactory.GetDriver("chrome");
 
-            // Report test set up
-            reportName = TestContext.CurrentContext.Test.Name;
+            // Report test set up.
+            var reportName = _context.Test.Name;
             _test = _extent.CreateTest(reportName);
         }
 
         [TearDown]
         public void TearDown()
         {
-            // Get test results, message, method name, and stack trace
-            var testStatus = TestContext.CurrentContext.Result.Outcome.Status;
-            var testMessage = TestContext.CurrentContext.Result.Message;
-            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
-            var testMethodName = TestContext.CurrentContext.Test.MethodName;
+            // Get test results, message, method name, and stack trace.
+            var testStatus = _context.Result.Outcome.Status;
+            var testMessage = _context.Result.Message;
+            var stackTrace = _context.Result.StackTrace;
+            var testMethodName = _context.Test.MethodName;
 
             TestResultHelper.LogTestResults(testStatus, _driver, _test, testMessage, stackTrace, testMethodName);
 
-            // Close driver
-            _driver.Quit();
-            _driver.Dispose();
+            // Close driver.
+
+            if (_driver != null)
+            {
+                WebDriverFactory.CloseDriver();
+                _driver = null;
+            }
         }
 
         [OneTimeTearDown]
         public void CloseReport()
         {
-            // Close report
+            // Close report.
             _extent.Flush();
         }
     }
