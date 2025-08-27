c8002d38ef7d709ac4f061a223f66e8c3de94c5a
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-27 14:37:10 +0800

    Add GetDriver, and CloseDriver methods in WebDriverFactory.

--- TestUtilities/WebDriverFactory.cs
+++ TestUtilities/WebDriverFactory.cs
@@ -6,39 +6,52 @@ namespace TestUtilities
 {
     public static class WebDriverFactory
     {
-        public static IWebDriver CreateDriver(string driver)
+        [ThreadStatic]
+        private static IWebDriver _driver;
+
+        // Creating the driver base on the browser; To be used base test file.
+        public static IWebDriver CreateDriver(string browser)
         {
-            switch (driver.ToLower())
+            switch (browser.ToLower())
             {
                 case "chrome":
                     var chromeOptions = new ChromeOptions();
-                    // Prevent "Chrome is being controlled by automated test software" infobar
                     chromeOptions.AddArgument("--disable-infobars");
-
-                    // Disable password manager popups
                     chromeOptions.AddArgument("--disable-save-password-bubble");
                     chromeOptions.AddArgument("--disable-password-manager-reauthentication");
-
-                    // Incognito
-                    chromeOptions.AddArgument("--incognito");
-
-                    // Maximized window
                     chromeOptions.AddArgument("start-maximized");
-
+                    // Start tests in incognito mode.
+                    chromeOptions.AddArgument("--incognito");
                     return new ChromeDriver(chromeOptions);
 
                 case "edge":
                     var edgeOptions = new EdgeOptions();
-                    // Maximized window
                     edgeOptions.AddArgument("start-maximized");
-
-                    // Headless 
-                    edgeOptions.AddArgument("--headless");
-
                     return new EdgeDriver(edgeOptions);
 
                 default:
                     throw new ArgumentException("Invalid browser name. Please enter 'chrome' or 'edge'.");
+            }
+        }
+
+        // Getting the driver; Default browser is chrome; To be used in BaseTest file.
+        public static IWebDriver GetDriver(string browser = "chrome")
+        {
+            if (_driver == null)
+            {
+                _driver = CreateDriver(browser);
+            }
+            return _driver;
+        }
+
+        // Closing the driver; To be used in BaseTest file.
+        public static void CloseDriver()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver.Dispose();
+                _driver = null;
             }
         }
     }
