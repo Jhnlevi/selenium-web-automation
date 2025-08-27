313118bad243f1b34bbb905bf1e7f2bd7f0f104a
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-27 14:09:40 +0800

    Add the existing utils to TestUtilities project.

---
+++ TestUtilities/ScreenshotHelper.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace SauceDemoTests.Utils
+{
+    public static class ScreenshotHelper
+    {
+        public static string CaptureScreenshot(IWebDriver driver, string testName, string data = "")
+        {
+            // Navigate up from bin to project folder
+            var projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\.."));
+            string screenshotDirectory = Path.Combine(projectRoot, "Reports", "Screenshots");
+
+            // Checks if the folder exists
+            if (!Directory.Exists(screenshotDirectory))
+            {
+                Directory.CreateDirectory(screenshotDirectory);
+            }
+
+            // Captures screenshot
+            ITakesScreenshot screenshotDriver = (ITakesScreenshot)driver;
+            var screenshot = screenshotDriver.GetScreenshot();
+
+            // Get timestamps and filepath
+            string timeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            var fileName = string.IsNullOrEmpty(data)
+                ? $"{testName}_{timeStamp}.png"
+                : $"{testName}_{data}_{timeStamp}.png";
+
+            var filePath = Path.Combine(screenshotDirectory, fileName);
+
+            screenshot.SaveAsFile(filePath);
+            //Console.WriteLine($"Screenshot saved at: {filePath}");
+
+            return Path.Combine("Screenshots", fileName);
+        }
+    }
+}
---
+++ TestUtilities/TestResultHelper.cs
@@ -0,0 +1,39 @@
+﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
+
+namespace SauceDemoTests.Utils
+{
+    public static class TestResultHelper
+    {
+        public static void LogTestResults(
+            NUnit.Framework.Interfaces.TestStatus status,
+            IWebDriver driver,
+            ExtentTest extentTest,
+            string message,
+            string stackTrace,
+            string methodName)
+        {
+            switch (status)
+            {
+                case NUnit.Framework.Interfaces.TestStatus.Failed:
+                    string screenshotFile = ScreenshotHelper.CaptureScreenshot(driver, TestContext.CurrentContext.Test.MethodName);
+                    extentTest.Fail($"Test Failed : {message}\n{stackTrace}").AddScreenCaptureFromPath(screenshotFile);
+                    break;
+                case NUnit.Framework.Interfaces.TestStatus.Passed:
+                    if (TestContext.CurrentContext.Test.Arguments.Contains("Negative"))
+                    {
+                        string passScreenshot = ScreenshotHelper.CaptureScreenshot(driver, TestContext.CurrentContext.Test.MethodName);
+                        extentTest.Pass("Test Passed").AddScreenCaptureFromPath(passScreenshot);
+                    }
+                    else
+                    {
+                        extentTest.Pass("Test Passed");
+                    }
+                    break;
+                default:
+                    extentTest.Warning("Test ended with unusual status");
+                    break;
+            }
+        }
+    }
+}
