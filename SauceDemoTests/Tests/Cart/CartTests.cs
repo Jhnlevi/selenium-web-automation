dc0e5ffafd8beaac75fd5c344108036cb8471a07
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-28 16:04:36 +0800

    Add test file for cart page.

---
+++ SauceDemoTests/Tests/Cart/CartTests.cs
@@ -0,0 +1,217 @@
+﻿using SauceDemoTests.Pages.Cart;
+using SauceDemoTests.Pages.Login;
+using SauceDemoTests.Pages.Product;
+
+namespace SauceDemoTests.Tests.Cart
+{
+    public class CartTests : BaseTest
+    {
+        private LoginPage _login;
+        private ProductPage _product;
+        private CartPage _cart;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            // Setup basetest methods first
+            base.SetUp();
+
+            // Initialize LoginPage, and ProductPage.
+            _login = new LoginPage(_driver);
+            _product = new ProductPage(_driver);
+            _cart = new CartPage(_driver);
+
+            // Navigate to SauceDemo Website.
+            _test.Info("Navigating to SauceDemo website.");
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/v1/");
+
+            // Log in to the website
+            _test.Info("Log in as standard user.");
+            _login.LoginAs("standard_user", "secret_sauce");
+        }
+
+        // TC_Cart_0001
+        [Test]
+        public void Cart_AddProductToCart()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Verifying that an item is added to cart.");
+            Assert.That(_product.GetRemoveFromCartBtnText, Is.EqualTo("REMOVE"));
+        }
+
+        // TC_Cart_0002
+        [Test]
+        public void Cart_RemoveProductFromCart()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking 'Remove' button to remove product.");
+            _product.ClickRemoveToCart();
+
+            _test.Info("Verifying that an item is removed from cart.");
+            Assert.That(_product.GetAddToCartBtnText, Is.EqualTo("ADD TO CART"));
+        }
+
+        // TC_Cart_0003
+
+        [Test]
+        public void Cart_VerifyCartIconUpdate()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Verifying that cart icon counter is updated with the correct item count.");
+            Assert.That(_cart.GetMenuCurrentItemCount, Is.GreaterThan(0));
+        }
+
+        // TC_Cart_0004
+        [Test]
+        public void Cart_VerifyItemCountPersists()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking product to open product details page.");
+            _product.ClickProduct();
+
+            _test.Info("Verifying that cart icon counter remains when changing a page.");
+            Assert.That(_cart.GetMenuCurrentItemCount, Is.GreaterThan(0));
+
+        }
+
+        // TC_Cart_0005
+        [Test]
+        public void Cart_VerifyProductQuantityInCartPage()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the quantity of product added to cart.");
+            Assert.That(_cart.GetCartCurrentItemCount, Is.GreaterThan(0));
+        }
+
+        // TC_Cart_0006
+        [Test]
+        public void Cart_VerifyProductNameInCartPage()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the name of product added to cart.");
+            _product.ShouldDisplayProductName();
+        }
+
+        // TC_Cart_0007
+        [Test]
+        public void Cart_VerifyProductPriceInCartPage()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the price of product added to cart.");
+            _product.ShouldDisplayProductPrice();
+        }
+
+        // TC_Cart_0008
+        [Test]
+        public void Cart_VerifyProductDescriptionInCartPage()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the description of product added to cart.");
+            _product.ShouldDisplayProductDescription();
+        }
+
+        // TC_Cart_0009
+        [Test]
+        public void Cart_VerifyContinueShoppingIsDisplayed()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the 'Continue Shopping' button.");
+            _cart.ShouldDisplayContinueShoppingButton();
+        }
+
+        // TC_Cart_0010
+        [Test]
+        public void Cart_VerifyCheckoutIsDisplayed()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the 'Checkout' button.");
+            _cart.ShouldDisplayCheckoutButton();
+        }
+
+        // No test case available.
+        [Test]
+        public void Cart_VerifyRemoveItemButtonIsDisplayed()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Verifying that user can see the 'Remove' button.");
+            _cart.ShouldDisplayRemoveButton();
+        }
+
+        // No test case available.
+        [Test]
+        public void Cart_VerifyContinueShoppingFunctionality()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Clicking 'Continue shopping' button.");
+            _cart.ClickContinueShoppingItem();
+
+            _test.Info("Verifying that the user is redirected to product list page.");
+            Assert.That(_driver.Url.Contains("inventory"), Is.True);
+        }
+
+        // No test case available.
+        [Test]
+        public void Cart_CheckoutFunctionality()
+        {
+            _test.Info("Clicking 'Add to Cart' button to add product.");
+            _product.ClickAddToCart();
+
+            _test.Info("Clicking shopping cart button to open cart page.");
+            _cart._menu.ClickCart();
+
+            _test.Info("Clicking 'Continu shopping' button.");
+            _cart.ClickCheckoutCartItem();
+
+            _test.Info("Verifying that the user is redirected to checkout step one page.");
+            Assert.That(_driver.Url.Contains("checkout-step-one"), Is.True);
+        }
+    }
+}
