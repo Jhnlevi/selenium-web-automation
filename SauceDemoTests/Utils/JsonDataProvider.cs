9e0046a966653f8dc1e89f13b6652dbb8b7ecff7
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-29 20:06:04 +0800

    Update json provider to use filtered test data.

--- SauceDemoTests/Utils/JsonDataProvider.cs
+++ SauceDemoTests/Utils/JsonDataProvider.cs
@@ -4,13 +4,31 @@ namespace SauceDemoTests.Utils
 {
     public static class JsonDataProvider
     {
-        public static IEnumerable<TestCaseData> GetLoginTestData(string testType, string model)
+        public static IEnumerable<TestCaseData> GetLoginTestData(string testType)
         {
             // Path for the login test data.
             var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Login", "loginTestData.json");
 
             // Call JsonDataReader util.
-            var data = JsonDataReader.ReadJson<List<LoginTestRoot>>(path);
+            var data = JsonDataReader.ReadJson<LoginTestRoot>(path);
+
+            // Filter test case.
+            var filteredTestCase = data.testCases
+                .Where(tc => tc.testType.Equals(testType, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            foreach (var testCase in filteredTestCase)
+            {
+                // return a single test case.
+                yield return new TestCaseData(testCase).SetName($"{testCase.testCaseId} - {testCase.testCaseDescription}"); ;
+            }
+
         }
+
+        // Method for calling positive login test cases.
+        public static IEnumerable<TestCaseData> LoginPositiveCases => GetLoginTestData("positive");
+
+        // Method for calling positive negative test cases.
+        public static IEnumerable<TestCaseData> LoginNegativeCases => GetLoginTestData("negative");
     }
 }
