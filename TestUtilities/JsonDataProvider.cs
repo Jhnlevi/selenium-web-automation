f3d06d357ef9109a101d1c59c2a2d9ed815cfeb2
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-29 18:14:20 +0800

    Add json reader and provider util classes.

---
+++ TestUtilities/JsonDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TestUtilities
+{
+    public static class JsonDataProvider
+    {
+        public static IEnumerable<TestCaseData> GetLoginTestData(string fileName)
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", fileName);
+            var data = JsonDataReader.ReadJson<List<>>(path);
+
+            foreach (var item in data)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ TestUtilities/JsonDataReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace TestUtilities
+{
+    public static class JsonDataReader
+    {
+        public static T ReadJson<T>(string filePath)
+        {
+            var data = File.ReadAllText(filePath);
+
+            return JsonSerializer.Deserialize<T>(data)!;
+        }
+    }
+}
