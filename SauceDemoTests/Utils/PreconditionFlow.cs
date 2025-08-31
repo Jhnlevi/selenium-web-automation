d08c56994a5ee32abea0a2c4304a4b3980ff1c22
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-08-31 18:46:10 +0800

    Add precondition test flow util file for all pre-conditions.

---
+++ SauceDemoTests/Utils/PreconditionFlow.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using SauceDemoTests.Pages.Cart;
+using SauceDemoTests.Pages.Login;
+using SauceDemoTests.Pages.MenuBar;
+using SauceDemoTests.Pages.Product;
+
+namespace SauceDemoTests.Utils
+{
+    public static class PreconditionFlow
+    {
+        public static void LoginAsStandardUser(IWebDriver driver, string username, string password)
+        {
+            var _login = new LoginPage(driver);
+            _login.EnterUserName(username);
+            _login.EnterPassword(password);
+            _login.ClickLoginBtn();
+        }
+        public static void AddProductToCart(IWebDriver driver)
+        {
+            var _product = new ProductPage(driver);
+            _product.ClickAddToCart();
+        }
+        public static void NavigateToCart(IWebDriver driver)
+        {
+            var _menu = new MenuBarPage(driver);
+            _menu.ClickCart();
+        }
+        public static void NavigateToCheckout(IWebDriver driver)
+        {
+            var _cart = new CartPage(driver);
+            _cart.ClickCheckoutCartItem();
+        }
+    }
+}
