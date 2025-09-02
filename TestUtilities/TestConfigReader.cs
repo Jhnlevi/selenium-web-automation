0316397eee07fb3a9b4f4142537f58d596002428
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-03 00:32:19 +0800

    Add util helper for reading app config.

---
+++ TestUtilities/TestConfigReader.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace TestUtilities
+{
+    public static class TestConfigReader
+    {
+        // Load the appsettings in appsetting.json file.
+        public static T GetAppSettings<T>(string fileName = "appsettings.json", string sectionName = "AppSettings")
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(TestContext.CurrentContext.TestDirectory)
+                .AddJsonFile(fileName, optional: false, reloadOnChange: true)
+                .Build();
+
+            return config.GetSection(sectionName).Get<T>()!;
+        }
+    }
+}
