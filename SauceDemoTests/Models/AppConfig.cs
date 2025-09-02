435999e660e655618c55de29bf9274d0926c8fe4
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-03 00:31:22 +0800

    Add AppConfig model for appsettings mapping.

---
+++ SauceDemoTests/Models/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace SauceDemoTests.Models
+{
+    public class AppConfig
+    {
+        public string BaseUrl { get; set; } = null!;
+        public string Browser { get; set; } = null!;
+        public int ImplicitWait { get; set; }
+        public int PageLoadTimeout { get; set; }
+        public string ScreenshotFolderPath { get; set; } = null!;
+        public string ReportFolderPath { get; set; } = null!;
+    }
+}
