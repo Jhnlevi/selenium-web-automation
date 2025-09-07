a368dfbce7546b2b3906955e44de4b4b7082b0b7
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-07 12:49:06 +0800

    Add TestDataProvider, and implement methods getting test data for login.

---
+++ OrangeHRMTests/Utils/TestDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using OrangeHRMTests.Models.Login;
+using TestUtilities;
+
+namespace OrangeHRMTests.Utils
+{
+    internal class TestDataProvider
+    {
+        // Get single login data.
+        public static TestCaseData GetSingleLoginRecord(string testId)
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Login", "loginTestData.json");
+            var data = TestDataReader.ReadJson<LoginTestModel>(path);
+            var testCase = data?.TestCases?.FirstOrDefault(tc => tc.TestCaseId == testId);
+
+            return new TestCaseData(testCase);
+        }
+
+        // Get multiple login data.
+        public static IEnumerable<TestCaseData> GetMultipleLoginRecord(string type)
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Login", "loginTestData.json");
+            var data = TestDataReader.ReadJson<LoginTestModel>(path);
+
+            var filteredTestCase = data?.TestCases?
+                .Where(tc => tc.TestType.Equals(type, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            foreach (var testCase in filteredTestCase)
+            {
+                yield return new TestCaseData(testCase);
+            }
+        }
+
+        // For negative tests of login.
+        public static IEnumerable<TestCaseData> LoginNegativeCases => GetMultipleLoginRecord("negative");
+    }
+}
