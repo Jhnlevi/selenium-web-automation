4b4748e622c1798296364c640352474b9330c559
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-08 23:50:14 +0800

    Modify LoginModel to override names of tests.

--- OrangeHRMTests/Models/Login/LoginModel.cs
+++ OrangeHRMTests/Models/Login/LoginModel.cs
@@ -6,8 +6,10 @@ namespace OrangeHRMTests.Models.Login
     {
         [JsonPropertyName("name")]
         public string Name { get; set; } = null!;
+
         [JsonPropertyName("description")]
         public string Description { get; set; } = null!;
+
         [JsonPropertyName("testCases")]
         public List<LoginCase>? TestCases { get; set; }
     }
@@ -15,19 +17,30 @@ namespace OrangeHRMTests.Models.Login
     {
         [JsonPropertyName("testCaseId")]
         public string TestCaseId { get; set; } = null!;
+
         [JsonPropertyName("testCaseDescription")]
         public string TestCaseDescription { get; set; } = null!;
+
         [JsonPropertyName("testType")]
         public string TestType { get; set; } = null!;
+
         [JsonPropertyName("testData")]
         public LoginData? TestData { get; set; }
+
         [JsonPropertyName("expectedResult")]
         public ExpectedResult? ExpectedResult { get; set; }
+
+        // Custom name for each test case.
+        public override string ToString()
+        {
+            return $"{TestCaseId} : {TestCaseDescription}";
+        }
     }
     internal class LoginData
     {
         [JsonPropertyName("userName")]
         public string UserName { get; set; } = null!;
+
         [JsonPropertyName("password")]
         public string Password { get; set; } = null!;
     }
@@ -35,6 +48,7 @@ namespace OrangeHRMTests.Models.Login
     {
         [JsonPropertyName("result")]
         public string Result { get; set; } = null!;
+
         [JsonPropertyName("message")]
         public string Message { get; set; } = null!;
     }
