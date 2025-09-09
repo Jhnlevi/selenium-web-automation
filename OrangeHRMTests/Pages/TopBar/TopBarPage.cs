647d3a3fe8adad09543ada88dbf8006305cc0d92
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-09 23:01:20 +0800

    Add TopBarPage class and implement locators/actions.

---
+++ OrangeHRMTests/Pages/TopBar/TopBarPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace OrangeHRMTests.Pages.TopBar
+{
+    internal class TopBarPage : BasePage
+    {
+        public TopBarPage(IWebDriver driver) : base(driver) { }
+
+        private By UserDropdownMenu = By.CssSelector(".oxd-userdropdown-tab");
+        private By BtnLogout = By.CssSelector("ul.oxd-dropdown-menu a.oxd-userdropdown-link[href=\'/web/index.php/auth/logout\']");
+
+        public void ClickUserDropdown() => Click(UserDropdownMenu);
+        public void ClickLogoutButton() => Click(BtnLogout);
+    }
+}
