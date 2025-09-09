f3256d6c5043eef34ec6ac28c90a9ec02f9727f6
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 06:57:06 +0800

    Add new method 'NavigateToMyInfo'.

--- OrangeHRMTests/Utils/TestFlow.cs
+++ OrangeHRMTests/Utils/TestFlow.cs
@@ -1,18 +1,32 @@
 ﻿using OpenQA.Selenium;
+using OrangeHRMTests.Constants;
 using OrangeHRMTests.Pages.Login;
+using OrangeHRMTests.Pages.Menu;
 
 namespace OrangeHRMTests.Utils
 {
     internal class TestFlow
     {
-        public static void LoginAsAdmin(IWebDriver driver, string username, string password, string url)
+        public static void LoginAsAdmin(IWebDriver driver, string url)
         {
             var login = new LoginPage(driver);
 
             login.NavigateToUrl(url);
-            login.EnterUsername(username);
-            login.EnterPassword(password);
+            login.EnterUsername(TestAccount.Username);
+            login.EnterPassword(TestAccount.Password);
             login.ClickLoginButton();
+        }
+
+        public void NavigateToMyInfo(IWebDriver driver, string url)
+        {
+            var login = new LoginPage(driver);
+            var menu = new NavigationMenuPage(driver);
+
+            login.NavigateToUrl(url);
+            login.EnterUsername(TestAccount.Username);
+            login.EnterPassword(TestAccount.Password);
+            login.ClickLoginButton();
+            menu.ClickElement(MenuFields.MyInfo);
         }
     }
 }
