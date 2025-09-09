227750447471ff5da5b4dad766266754b7db1043
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 07:50:21 +0800

    Add constant file for profile tabs.

---
+++ OrangeHRMTests/Constants/ProfileTabs.cs
@@ -0,0 +1,9 @@
+﻿namespace OrangeHRMTests.Constants
+{
+    internal static class ProfileTabs
+    {
+        public const string PersonalDetails = "PersonalDetails";
+        public const string ContactDetails = "ContactDetails";
+        public const string EmergencyContacts = "EmergencyContacts";
+    }
+}
