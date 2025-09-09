cb27ae6262dae11afdeaf4c6df596f8e9135c6ca
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 04:27:36 +0800

    Add 'TestFlow' util class, and implement 'LoginAdAdmin' method.

---
+++ OrangeHRMTests/Utils/TestFlow.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OrangeHRMTests.Pages.Login;
+
+namespace OrangeHRMTests.Utils
+{
+    internal class TestFlow
+    {
+        public static void LoginAsAdmin(IWebDriver driver, string username, string password, string url)
+        {
+            var login = new LoginPage(driver);
+
+            login.NavigateToUrl(url);
+            login.EnterUsername(username);
+            login.EnterPassword(password);
+            login.ClickLoginButton();
+        }
+    }
+}
