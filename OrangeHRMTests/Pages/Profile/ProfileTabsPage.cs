f857d58ba94e65de28fd56cd2f209d5009bbb4f4
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 07:50:34 +0800

    Add POM file for profile tabs.

---
+++ OrangeHRMTests/Pages/Profile/ProfileTabsPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OrangeHRMTests.Constants;
+
+namespace OrangeHRMTests.Pages.Profile
+{
+    internal class ProfileTabsPage : BasePage
+    {
+        // Dictionary for all elements.
+        private readonly Dictionary<string, By> _elements;
+
+        public ProfileTabsPage(IWebDriver driver) : base(driver)
+        {
+            _elements = new Dictionary<string, By>
+            {
+                { ProfileTabs.PersonalDetails, PersonalDetailsTab },
+                { ProfileTabs.ContactDetails, ContactDetailsTab },
+                { ProfileTabs.EmergencyContacts, EmergencyContactsTab }
+            };
+        }
+
+        private By PersonalDetailsTab = By.LinkText("Personal Details");
+        private By ContactDetailsTab = By.LinkText("Contact Details");
+        private By EmergencyContactsTab = By.LinkText("Emergency Contacts");
+
+        // Checking if element is displayed.
+        public bool IsDisplayed(string fieldName)
+        {
+            if (!_elements.ContainsKey(fieldName))
+                throw new ArgumentException($"Field '{fieldName}' not found in dictionary.");
+
+            return IsElementDisplayed(_elements[fieldName]);
+        }
+
+        // Clicking an element.
+        public void ClickElement(string fieldName) => Click(_elements[fieldName]);
+    }
+}
