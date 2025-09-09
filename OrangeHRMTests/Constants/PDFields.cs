9a4d4e44ada1284f93fc3573cf6c7d9d107115f9
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 06:41:48 +0800

    Add PDFields class for fields constants.

---
+++ OrangeHRMTests/Constants/PDFields.cs
@@ -0,0 +1,19 @@
+﻿namespace OrangeHRMTests.Constants
+{
+    internal static class PDFields
+    {
+        public const string FirstName = "FirstName";
+        public const string MiddleName = "MiddleName";
+        public const string LastName = "LastName";
+        public const string EmployeeId = "EmployeeId";
+        public const string OtherId = "OtherId";
+        public const string DriversNumber = "DriverLicenseNumber";
+        public const string LicenseExpiryDate = "LicenseExpiryDate";
+        public const string Nationality = "Nationality";
+        public const string MaritalStatus = "MaritalStatus";
+        public const string DateOfBirth = "DateOfBirth";
+        public const string MaleRadio = "MaleRadio";
+        public const string FemaleRadio = "FemaleRadio";
+        public const string SaveButton = "SaveButton";
+    }
+}
