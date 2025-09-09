643faa4e86676e75070cc0a684b160e38963fabe
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-10 04:31:55 +0800

    Add 'NavigationMenuPage' class.

---
+++ OrangeHRMTests/Pages/Menu/NavigationMenuPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace OrangeHRMTests.Pages.Menu
+{
+    internal class NavigationMenuPage : BasePage
+    {
+        public NavigationMenuPage(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
