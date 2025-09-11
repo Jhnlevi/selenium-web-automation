21d7ad5550e7858d4c782eaab3e923be8ccc209d
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-11 10:08:10 +0800

    Move FieldType enum to its own folder.

--- OrangeHRMTests/Models/Profile/Field_PD_Model.cs
+++ OrangeHRMTests/Models/Profile/Field_PD_Model.cs
@@ -1,18 +1,11 @@
-﻿namespace OrangeHRMTests.Models.Profile
+﻿using OrangeHRMTests.Enums;
+
+namespace OrangeHRMTests.Models.Profile
 {
     internal class Field_PD_Model
     {
         public string Locator { get; set; } = null!;
         public string Value { get; set; } = null!;
         public FieldType Type { get; set; }
-    }
-
-    internal enum FieldType
-    {
-        Text,
-        Dropdown,
-        Date,
-        Radio,
-        Button
     }
 }
