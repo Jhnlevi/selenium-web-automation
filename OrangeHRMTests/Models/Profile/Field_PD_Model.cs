85a4c9c10db6031b6be63a2e9645e12f3dd4a421
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-11 10:02:23 +0800

    Add field model for 'Personal Details' tab to use in test methods.

---
+++ OrangeHRMTests/Models/Profile/Field_PD_Model.cs
@@ -0,0 +1,18 @@
+﻿namespace OrangeHRMTests.Models.Profile
+{
+    internal class Field_PD_Model
+    {
+        public string Locator { get; set; } = null!;
+        public string Value { get; set; } = null!;
+        public FieldType Type { get; set; }
+    }
+
+    internal enum FieldType
+    {
+        Text,
+        Dropdown,
+        Date,
+        Radio,
+        Button
+    }
+}
