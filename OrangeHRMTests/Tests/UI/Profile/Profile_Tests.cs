1b2bf2c01813cf3845cf107e9b0eeae04778c66e
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-11 08:38:16 +0800

    Implement test methods in Profile_Tests.

--- OrangeHRMTests/Tests/UI/Profile/Profile_Tests.cs
+++ OrangeHRMTests/Tests/UI/Profile/Profile_Tests.cs
@@ -8,26 +8,44 @@ namespace OrangeHRMTests.Tests.UI.Profile
     internal class Profile_Tests : BaseTest
     {
         private ProfilePDPage _profilePD;
+        private ProfilePage _profile;
 
         [SetUp]
         public override void Setup()
         {
             base.Setup();
             _profilePD = new ProfilePDPage(_driver!);
+            _profile = new ProfilePage(_driver!);
 
-            ReportManager.LogInfo("Navigating and logging in (As Admin) to OrangeHRM demo website.");
-            TestFlow.LoginAsAdmin(_driver!, _config.BaseUrl);
+            ReportManager.LogInfo("Logging in to OrangeHRM website, and navigating to 'My Info' Page.");
+            TestFlow.Login_MyInfo_Flow(_driver!, _config.BaseUrl);
         }
 
         [Test]
         public void Profile_PersonalDetails_IsAccessible()
         {
-            ReportManager.LogInfo("Navigating to MyInfo page.");
-            _profilePD._menu.ClickElement(Fields_Menu.MyInfo);
             ReportManager.LogInfo("Navigating to 'Personal Details' tab.");
-            _profilePD._profileTab.ClickElement(Fields_Profile.PersonalDetails);
+            _profile.ClickElement(Fields_Profile.PersonalDetails);
             ReportManager.LogInfo("Verify that 'Personal Details' tab loads successfully.");
-            Assert.That(_profilePD._profileTab.IsDisplayed(Fields_Profile.PersonalDetails), Is.True, "'Personal Details tab does not load.");
+            Assert.That(_profile.IsDisplayed(Fields_Profile.PersonalDetails), Is.True, "'Personal Details' tab does not load.");
+        }
+
+        [Test]
+        public void Profile_ContactDetails_IsAccessible()
+        {
+            ReportManager.LogInfo("Navigating to 'Contact Details' tab.");
+            _profile.ClickElement(Fields_Profile.ContactDetails);
+            ReportManager.LogInfo("Verify that 'Contact Details' tab loads successfully.");
+            Assert.That(_profile.IsDisplayed(Fields_Profile.PersonalDetails), Is.True, "'Contact Details' tab does not load.");
+        }
+
+        [Test]
+        public void Profile_EmergencyContacts_IsAccessible()
+        {
+            ReportManager.LogInfo("Navigating to 'Contact Details' tab.");
+            _profile.ClickElement(Fields_Profile.EmergencyContacts);
+            ReportManager.LogInfo("Verify that 'Contact Details' tab loads successfully.");
+            Assert.That(_profile.IsDisplayed(Fields_Profile.EmergencyContacts), Is.True, "'Emergency Contacts' tab does not load.");
         }
     }
 }
