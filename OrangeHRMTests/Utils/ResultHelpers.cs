851d5099a8f75f1af952516bddcaafa13cf471d9
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-18 08:39:09 +0800

    feat: implement 'LogResults' method in ResultHelpers class.

--- OrangeHRMTests/Utils/ResultHelpers.cs
+++ OrangeHRMTests/Utils/ResultHelpers.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using SeleniumToolkit.Helpers;
 
 namespace OrangeHRMTests.Utils
 {
-    internal class ResultHelpers
+    internal static class ResultHelpers
     {
+        /// <summary>
+        /// Log test results to report.
+        /// </summary>
+        public static void LogResults(
+            IWebDriver driver,
+            string status,
+            string message,
+            string trace,
+            string name)
+        {
+            switch (status)
+            {
+                case "Passed":
+                    ReportManager.LogPass("Test passed!");
+                    break;
+                case "Failed":
+                    string screenshotPath = ScreenshotManager.Capture(driver, name);
+                    ReportManager.LogFail($"Test Failed: {message}");
+                    ReportManager.LogInfo($"Stack trace: {trace}");
+                    ReportManager.ReportAttachScreenshot(screenshotPath);
+                    break;
+                default:
+                    ReportManager.LogWarn("Test ended with unusual status...");
+                    break;
+            }
+        }
     }
 }
